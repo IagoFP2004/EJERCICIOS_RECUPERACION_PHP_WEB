22ec8ddee46b214f89e56bedb90443ba4fcf9157
IagoFP <iagofranciscoperez123@gmail.com>
2025-05-12 18:07:30 +0200

    Listado soporta paginacion y ordenacion filtrado finalizado

--- www/app/Controllers/CategoriaController.php
+++ www/app/Controllers/CategoriaController.php
@@ -3,6 +3,7 @@
 namespace Com\Daw2\Controllers;
 
 use Com\Daw2\Core\BaseController;
+use Com\Daw2\Models\CategoriaModel;
 
 class CategoriaController extends BaseController
 {
@@ -13,6 +14,55 @@ class CategoriaController extends BaseController
             'breadcrumb' => ['Inicio'],
             'seccion' => '/inicio/categorias'
         );
+
+        $model = new CategoriaModel();
+
+        $copia_GET= $_GET;
+        unset($copia_GET['order']);
+        $data['url']=http_build_query($copia_GET);
+
+        $data['order'] = $this->getOrder();
+
+        $copia_GET2= $_GET;
+        unset($copia_GET2['page']);
+        $data['url2']=http_build_query($copia_GET2);
+
+        $numeroRegistros = $model->countResults();
+        $numeroPaginas = $this->getNumberPage($numeroRegistros);
+        $data['page'] = $this->getPage($numeroPaginas);
+        $data['max_page'] = $numeroPaginas;
+
+
+
+        $data['listado'] = $model->get($_GET, $data['order'], $data['page']);
+
         $this->view->showViews(array('templates/header.view.php', 'categorias.view.php', 'templates/footer.view.php'), $data);
+    }
+
+    public function getOrder():int
+    {
+        if (isset($_GET['order']) && is_numeric($_GET['order'])) {
+            $order = (int) $_GET['order'];
+            if (abs($order) >= 1 && abs($order) <= 3) {
+                return $order;
+            }
+        }
+        return 1;
+    }
+
+    public function getNumberPage(int $numeroRegistros):int
+    {
+        return (int) ceil($numeroRegistros / $_ENV['numero.pagina']);
+    }
+
+    public function getPage(int $numeroPaginas):int
+    {
+        if (isset($_GET['page']) && is_numeric($_GET['page'])) {
+            $page = (int) $_GET['page'];
+            if ($_GET['page'] > 1 && $_GET['page'] <= $numeroPaginas) {
+                return (int) $_GET['page'];
+            }
+        }
+        return 1;
     }
 }
--- www/app/Models/CategoriaModel.php
+++ www/app/Models/CategoriaModel.php
@@ -6,5 +6,66 @@ use Com\Daw2\Core\BaseDbModel;
 
 class CategoriaModel extends BaseDbModel
 {
+    public const ORDER_COLUMN = ['nombre_categoria','nombre_completo_categoria','numero_articulos'];
+    public function get(array $data, int $order, int $page):array
+    {
+       $sentido = ($order > 0) ? 'ASC' : 'DESC';
+       $order = abs($order);
 
+       $limite = $_ENV['numero.pagina'];
+
+        $condiciones = [];
+        $condicionesHaving = [];
+        $valores = [];
+
+        if (!empty($data['nombre'])) {
+            $condiciones[] = "c.nombre_categoria LIKE :nombre";
+            $valores["nombre"] = "%" . $data['nombre'] . "%";
+        }
+
+        if (!empty($data['min_numero_articulos'])) {
+            $condicionesHaving[] = "numero_articulos >= :min_numero_articulos";
+            $valores["min_numero_articulos"] = $data['min_numero_articulos'];
+        }
+
+        if (!empty($data['max_numero_articulos'])) {
+            $condicionesHaving[] = "numero_articulos <= :max_numero_articulos";
+            $valores["max_numero_articulos"] = $data['max_numero_articulos'];
+        }
+
+        $sql = "SELECT 
+                c.nombre_categoria AS nombre_categoria,
+                CONCAT_WS(' > ', c3.nombre_categoria, c2.nombre_categoria, c.nombre_categoria) AS nombre_completo_categoria,
+                COUNT(DISTINCT p.id_producto) as numero_articulos
+            FROM categoria c 
+            LEFT JOIN categoria c2 ON c.id_padre = c2.id_categoria
+            LEFT JOIN categoria c3 ON c2.id_padre = c3.id_categoria
+            LEFT JOIN producto p ON p.id_categoria = c.id_categoria ";
+        if (!empty($condiciones)) {
+            $sql .= " WHERE ".implode(' AND ', $condiciones);
+        }
+        $sql.= " GROUP BY c.id_categoria";
+        if (!empty($condicionesHaving)) {
+            $sql .= " HAVING ".implode(' AND ', $condicionesHaving);
+        }
+        $sql.= " ORDER BY ".self::ORDER_COLUMN[$order-1]." ".$sentido;
+        $sql.= " LIMIT ".($page-1)*$limite.",".$limite;
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($valores);
+        return $stmt->fetchAll();
+    }
+
+    public function countResults():int
+    {
+        $sql = "SELECT 
+                COUNT(c.id_categoria)
+            FROM categoria c 
+            LEFT JOIN categoria c2 ON c.id_padre = c2.id_categoria
+            LEFT JOIN categoria c3 ON c2.id_padre = c3.id_categoria
+            LEFT JOIN producto p ON p.id_categoria = c.id_categoria ";
+        $sql.= " GROUP BY c.id_categoria";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchColumn();
+    }
 }
